cb63e8d555f9cf8cfc104581c09ca3bc69676096
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-08-29 13:53:51 -0400

    Basic features complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,41 +1,74 @@
 import React from "react"
+import redX from "../redX.png"
 
 export default function Question(props){
+    // console.log("Rendering Question")
+    // console.log("props.revealAnswers " + props.revealAnswers)
+    // console.log("Opposite " + !props.revealAnswers)
+
     const [answerArray, setAnswerArray] = React.useState([])
+    // const [showX, setShowX] = React.useState(false)
+    let showX = false
     const answerButtons = answerArray.map((answer, index) => {
+        let colorClass = "btn-outline-secondary"
+        
+        if (props.revealAnswers === true){
+            if (answer.isCorrect)
+                colorClass = "btn-success"
+            else if (props.selected_answer === answer.value)
+                colorClass = "btn-danger"
+                // setShowX(prevState => prevState = true)
+                showX = true
+        }
+
         const id = `question-${props.id}-answer-${index}`
+        
         return (
             <>
                 <input
-                    key={id}
                     id={id}
-                    name={`question-${props.id}`}
+                    name={props.id}
                     type="radio"
-                    className="btn-check"
+                    className={`btn-check`}
+                    onChange={() => props.handleSelectAnswer(props.id, answer.value)}
                 />
-                <label className="btn btn-outline-secondary mx-3" htmlFor={id}>{answer.value}</label>
+                <label
+                    className={`btn mx-3 ${colorClass} ${props.revealAnswers ? "disable-clicks" : ""}`}
+                    htmlFor={id}
+                >
+                    {answer.value}
+                </label>
             </>
         )
     })
 
     React.useEffect(() => {
-        const newArray = [...props.incorrect_answers]
-        let rand = Math.floor( Math.random() * newArray.length )
+        console.log("Question useEffect")
+        setAnswerArray(prevState => prevState = [])
         
-        newArray.splice(rand, 0, props.correct_answer)
-        
-        setAnswerArray(prevState => newArray.map(item => {
-            return {
-                value: item,
-                status: "unselected",
+        // console.log("Answer array reset: " + JSON.stringify(answerArray))
+
+        const newArray = props.incorrect_answers.map(answer => (
+            {
+                value: answer,
+                isCorrect: false,
             }
-        }))
-    }, [])
+        ))
+
+        let rand = Math.floor( Math.random() * newArray.length )
+        newArray.splice(rand, 0, {value: props.correct_answer, isCorrect: true})
+        
+        setAnswerArray(prevState => prevState = newArray)
+        // console.log("Answer array new: " + JSON.stringify(answerArray))
+    }, [props.gameReset])
 
     return (
         <>
             <div className="question">
-                <h5>{props.question}</h5>
+                <h5>
+                    {props.revealAnswers === true && props.selected_answer !== props.correct_answer && <img src={redX} alt="Answer incorrect" className="red-x me-3"></img>}
+                    {props.question}
+                </h5>
                 <div className="answer-set my-4">
                     {answerButtons}
                 </div>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,35 +4,75 @@ import React from "react"
 import Question from "./Question"
 
 export default function Quiz(){
+    // console.log("Render Quiz")
 
+    const [gameReset, setGameReset] = React.useState(false)
+    const [numCorrect, setNumCorrect] = React.useState(0)
+    const [revealAnswers, setRevealAnswers] = React.useState(false)
     const [questions, setQuestions] = React.useState([])
+    const allQuestionsAnswered = questions.every(question => question.selectedAnswer !== "")
     const questionElements = questions.map((question, index) => {
         return (
             <Question
                 key={index}
-                id={index}
+                id={question.id}
                 question={question.question}
                 correct_answer={question.correct_answer}
                 incorrect_answers={question.incorrect_answers}
+                selected_answer={question.selectedAnswer}
+                handleSelectAnswer={handleSelectAnswer}
+                revealAnswers={revealAnswers}
+                gameReset={gameReset}
             />
         )
     })
 
-    function checkAnswers(event){
-        console.log("PLACEHOLDER - Checking answers")
-        event.preventDefault()
+    function handleSelectAnswer(questionId, answer){
+        // console.log(`ID: ${questionId}\nAnswer: ${answer}`)
+
+        setQuestions(prevQuestions => prevQuestions.map(question => {
+            return question.id === questionId ? 
+            {...question, selectedAnswer: answer} :
+            question
+        }))
+    }
+
+    function checkAnswers(){
+        let count = 0
+
+        questions.forEach(question => {
+            // console.log(`${question.id}: ${question.selectedAnswer}`)
+            if (question.selectedAnswer === question.correct_answer)
+                count += 1
+        })
+
+        setRevealAnswers(prevState => prevState=true)
+        setNumCorrect(prevCount => prevCount = count)
+    }
+
+    function resetGame(){
+        setGameReset(prevState => prevState=true)
     }
 
     React.useEffect(() => {
+        // console.log("Before:" + questions)
+        setGameReset(prevState => prevState=false)
+        setNumCorrect(prevState => prevState=0)
+        setRevealAnswers(prevState => prevState=false)
+        // setQuestions(prevState => prevState=[])
+    
+        // console.log("Reset: " + questions)
+
+
         fetch("https://opentdb.com/api.php?amount=5&encode=base64")
             .then(response => response.json())
             .then(result => {
                 
                 const resultsArray = result.results
-                console.log(resultsArray)
+                // console.log(resultsArray)
 
                 //Finds each string in results, decodes from ASCII to binary
-                resultsArray.forEach(question => {
+                resultsArray.forEach((question, index) => {
                     for (let key in question){
                         if(Object.hasOwn(question, key)){
                             if (typeof question[key] == "string")
@@ -44,19 +84,35 @@ export default function Quiz(){
                                 }
                         }
                     }
+
+                    question['id'] = `question-${index}`
+                    question['selectedAnswer'] = ""
                 })
 
                 setQuestions(prevState => prevState = resultsArray)
             })
-    }, [])
+
+        // console.log("After: " + questions)
+    }, [gameReset])
+
+    // React.useEffect(() => {
+    //     console.log("Questions reset")
+    // }, [questions])
 
     return (
-        <form id="quiz" className="p-3">
+        <div id="quiz" className="p-3">
             <div id="question-list">
                 {questionElements}
             </div>
 
-            <button onClick={event => checkAnswers(event)} className="btn btn-lg btn-primary">SUBMIT</button>
-        </form>
+            {revealAnswers ?
+                <div className="d-flex">
+                    <button onClick={resetGame} className="btn btn-lg btn-warning me-5">Play Again?</button>
+                    <h1>You got {numCorrect} correct!</h1>
+                </div> :
+
+                <button onClick={checkAnswers} className="btn btn-lg btn-warning" disabled={!allQuestionsAnswered}>SUBMIT</button>
+            }
+        </div>
     )
 }
