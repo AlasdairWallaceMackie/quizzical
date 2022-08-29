21aa76e4938b8689d1de6d46d208d98028cae7a4
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-08-29 13:53:26 -0400

    Add sub title

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ export default function StartScreen(props){
         <div id="start-screem">
             <div className="position-absolute top-50 start-50 translate-middle text-center">
                 <h1 className="display-1">Quizzical</h1>
-                <h4 className="text-muted">Description Goes Here</h4>
+                <h4 className="text-muted">Are you a trivia master?</h4>
                 <br/>
                 <button className="btn btn-lg btn-success" onClick={props.onClick}>Start Quiz</button>
             </div>
